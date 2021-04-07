7c0908f50c767df1d14d2f5470fe16fd826e0430
Daniel Welsh <danjwelsh24@gmail.com>
2021-04-07 15:19:41 +0200

    removing old styleguide wrapper

--- src/styleguide/ThemeWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ThemeProvider } from "styled-components";
-import { LightTheme } from "../theme/theme";
-
-const ThemeWrapper = ({ children }) => {
-  return <ThemeProvider theme={LightTheme}>{children}</ThemeProvider>;
-};
-
-export default ThemeWrapper;
